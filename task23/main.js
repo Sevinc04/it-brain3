32d3a456f8ceeba675d13099c078fe308b2a9fab
Sevinc04 <vlizadsevinc78@gmail.com>
2024-01-12 17:36:40 +0400

    changed

---
+++ task23/main.js
@@ -0,0 +1,42 @@
+//?task1
+//let userName = 'My Name is Sevinc'
+//alert(userName);
+//console.log(userName);
+
+//?task2
+//const userName = 'Sevinc'
+//console.log(userName);
+
+//?task3
+//let cavab = 'person and $add'
+//console.log(cavab);
+
+//?task4
+//let bookPrice=750;
+//let bookAmount=14;
+//let totalPrice= bookPrice*bookAmount;
+//console.log(totalPrice);
+
+//?task5
+//let kmhSpeed = 75
+//let msSpeed = 75 * 10 / 36
+//console.log(msSpeed);
+
+//?task6
+// let year = prompt("Dogum ilini daxil et!");
+// const date = new Date()
+// let nowYear = date.getFullYear();
+// let age = nowYear - year;
+// console.log('Bugune qeder yashadiginiz gunlerin sayi: ' + age * 365);
+
+//?task7
+// let a = prompt("Reqem daxil edin.");
+// console.log(a**5);
+// console.log(a%3);
+
+//?task8
+// let firstName = 'Sevinc';
+// let lastName = 'Valizada';
+// console.log(`Sizin adiniz ${firstName} ${lastName} .Tanisdigimza çox şadam!`);
+
+//?task9
